efd5563ec60f4fa18ba58944b4de651b965a2cdb
Admin <riverflow1928@yandex.ru>
2021-03-26 17:39:02 +0500

    add mobx and react-router-dom

---
+++ front/src/components/progress/progress.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Progress() {
+    return (
+        <React.Fragment>
+            <h1>Course progress</h1>
+            <Link to="/">Homepage</Link>
+        </React.Fragment>
+    );
+}
+
+export default Progress;
