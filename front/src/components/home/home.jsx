6b35f9c5075afd49476b40d366cfe0ebacc64757
Admin <riverflow1928@yandex.ru>
2021-03-29 02:53:50 +0500

    add mobx-react-lite and global store

--- front/src/components/home/home.jsx
+++ front/src/components/home/home.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import store from "../../store/store";
 
-function Home() {
-    return (
-        <React.Fragment>
-            <h1>Homepage</h1>
-            <ul>
-                <li>
-                    <Link to="/settings">Settings</Link>
-                </li>
-                <li>
-                    <Link to="/progress">Course progress</Link>
-                </li>
-            </ul>
-        </React.Fragment>
-    );
-}
+const Home = observer(() =>
+    <React.Fragment>
+        <h1>Homepage</h1>
+        <ul>
+            <li>
+                <Link to="/settings" onClick={store.updateLinkClicksCount}>Settings</Link>
+            </li>
+            <li>
+                <Link to="/progress" onClick={store.updateLinkClicksCount}>Course progress</Link>
+            </li>
+        </ul>
+        <h2>Link clicks: {store.linkClicksCount}</h2>
+    </React.Fragment>
+);
 
 export default Home;
--- front/src/components/settings/settings.jsx
+++ front/src/components/settings/settings.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { observer } from "mobx-react-lite";
+import store from "../../store/store";
 
-function Settings() {
-    return (
-        <React.Fragment>
-            <h1>Settings</h1>
-            <Link to="/">Homepage</Link>
-        </React.Fragment>
-    );
-}
+const Settings = observer(() =>
+    <React.Fragment>
+        <h1>Settings</h1>
+        <Link to="/" onClick={store.updateLinkClicksCount}>Homepage</Link>
+        <h2>Link clicks: {store.linkClicksCount}</h2>
+    </React.Fragment>
+);
 
 export default Settings;
