feb4035ef9f6b31509f5fa44445f3ae634b58aa2
devlsay <devlsay@yandex.com>
2021-03-28 14:33:18 +0300

    add start pages

---
+++ front/src/components/settings/settings.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Settings() {
+    return (
+        <React.Fragment>
+            <h1>Settings</h1>
+            <Link to="/">Homepage</Link>
+        </React.Fragment>
+    );
+}
+
+export default Settings;
---
+++ front/src/router.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from "react-router-dom";
+import Home from "./components/home/home";
+import Settings from "./components/settings/settings";
+import Progress from "./components/progress/progress";
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/settings">
+                    <Settings />
+                </Route>
+                <Route path="/progress">
+                    <Progress />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Router;
