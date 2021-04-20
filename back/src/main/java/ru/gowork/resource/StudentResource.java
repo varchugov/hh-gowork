eb282bcb6f14bbecca29bd95d3d9fac4134d9488
Nikita Kalyanov <nikitakalyanov@gmail.com>
2021-04-21 00:36:21 +0300

    GW-51 add openapi (swagger) doc generation

--- back/src/main/java/ru/gowork/resource/StudentResource.java
+++ back/src/main/java/ru/gowork/resource/StudentResource.java
@@ -4,10 +4,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.WebApplicationException;
 
@@ -27,18 +27,17 @@ public class StudentResource {
         this.service = service;
     }
 
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @POST
-    public Response createStudent(@FormParam("username") String username) {
-        StudentDto student = service.createStudent(username);
-        return Response.status(200).entity(student).build();
+    public StudentDto createStudent(@FormParam("username") String username) {
+        return service.createStudent(username);
     }
 
     @Path("/{student_id}")
     @GET
-    public Response getStudent(@PathParam("student_id") Integer id) throws WebApplicationException {
+    public StudentDto getStudent(@PathParam("student_id") Integer id) throws WebApplicationException {
         try {
-            StudentDto student = service.getStudentById(id);
-            return Response.status(200).entity(student).build();
+            return service.getStudentById(id);
         } catch (StudentNotFoundException e) {
             throw new WebApplicationException("student with the specified id was not found", Status.NOT_FOUND);
         }
