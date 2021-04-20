f93dca7fd74a37579252b0d1977a76f0d7fc5bd9
devlsay <devlsay@yandex.com>
2021-04-20 22:00:31 +0300

    [GW-31] Add Golearn component

---
+++ front/src/components/shared/Golearn.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Button, Card, CardContent, CardActions, Typography } from '@material-ui/core';
+
+const GolearnCard = styled(Card)`
+    margin: 25px auto;
+    padding: 15px;
+    min-width: 240px;
+    max-width: 440px;
+    background-color: #0e4da4;
+`;
+
+const GolearnCardContent = styled(CardContent)`
+    color: #fff;
+`;
+
+const GolearnCardActions = styled(CardActions)`
+    display: flex;
+    justify-content: space-around;
+`;
+
+function Golearn() {
+    const iLesson = 1;
+    const countLessons = 11;
+
+    return (
+        <GolearnCard>
+            <GolearnCardContent>
+                <Typography gutterBottom>
+                    Урок {iLesson} из {countLessons}
+                </Typography>
+                <Typography variant="h6" component="h2" align="center">
+                    Как искать и находить <br /> работу мечты
+                </Typography>
+            </GolearnCardContent>
+            <GolearnCardActions>
+                <Button size="large" variant="contained">
+                    Учиться
+                </Button>
+            </GolearnCardActions>
+        </GolearnCard>
+    );
+}
+
+export default Golearn;
